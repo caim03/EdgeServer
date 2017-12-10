cbc34dba35e0f1404bf893822c6ece19c66b75df
zippo92 <simone.zippo@gmail.com>
2017-12-10 11:03:06 +0100

    Delete s3Configuration.js

--- config/s3Configuration.js
+++
@@ -1,7 +0,0 @@
-var s3Config = {
-    bucketName: "fogcomputingbucket",
-    accessKeyID: "AKIAJ7EUOJC2R27NGKIA",
-    secretAccessKeyId: "NQOw/fEu1Skd/rHvKJKf0ZiGBV+gIyPks5migTYB"
-};
-
-module.exports = s3Config;
